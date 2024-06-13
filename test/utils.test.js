eaa533014a47058e152b021c208807094f9b25d6
黎伟杰 <674416404@qq.com>
2024-06-13 11:25:08 +0800

    ci: wip

--- test/utils.test.js
+++ test/utils.test.js
@@ -1,12 +1,13 @@
-import { describe, expect, it } from 'vitest';
-import { parseJson, parseYaml } from '../src/utils';
+import { describe, expect, it } from 'vitest'
+import { parseJson, parseYaml } from '../src/utils'
+
 describe('utils', () => {
-    it('parseJson', () => {
-        const version = parseJson('test/fixtures/json/package.json');
-        expect(version).toBe('0.1.0');
-    });
-    it('parseYaml', () => {
-        const version = parseYaml('test/fixtures/yaml/pubspec.yaml');
-        expect(version).toBe('0.1.0');
-    });
-});
+  it('parseJson', () => {
+    const version = parseJson('test/fixtures/version/package.json')
+    expect(version).toBe('0.1.0')
+  })
+  it('parseYaml', () => {
+    const version = parseYaml('test/fixtures/version/pubspec.yaml')
+    expect(version).toBe('0.1.0')
+  })
+})
